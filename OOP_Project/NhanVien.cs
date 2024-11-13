752ddae7a34632a5648ef3307951a1a1526fe7c2
tnKiness <Khanhks2080@gmail.com>
2024-11-13 22:38:46 +0700

    Add project

---
+++ OOP_Project/NhanVien.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Project
+{
+    abstract class NhanVien
+    {
+
+        public DateTime NgayBatDauLam { get; set; }
+        public abstract int LuongCoBan();
+    }
+}
